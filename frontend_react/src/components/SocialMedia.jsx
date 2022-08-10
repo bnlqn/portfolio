cdb1db690b12aa03d6ca0ea78dbb1d8aff1b9338
Benjamin Lurquin <blurquin@gmail.com>
2022-08-10 22:26:43 +0200

    Improve some assets

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { BsTwitter, BsLinkedin } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { BsLinkedin } from 'react-icons/bs';
 
 const SocialMedia = () => (
   <div className="app__social">
-    <a href="https://www.linkedin.com/in/blurquin" target="_blank">
+    <a href="https://www.linkedin.com/in/blurquin" target="_blank" rel="noreferrer">
       <div>
         <BsLinkedin />
       </div>
