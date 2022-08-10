3c33e92c22d9e2dd64ea9efcf80bc86b6035b789
Benjamin Lurquin <blurquin@gmail.com>
2022-08-10 15:34:35 +0200

    Initial commit

---
+++ frontend_react/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
+import './App.scss';
+
+const App = () => (
+  <div className="app">
+    <Navbar />
+    <Header />
+    <About />
+    <Testimonial />
+    <Skills />
+    <Footer />
+  </div>
+);
+
+export default App;
---
+++ frontend_react/src/components/SocialMedia.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BsTwitter, BsLinkedin } from 'react-icons/bs';
+import { FaFacebookF } from 'react-icons/fa';
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <a href="https://www.linkedin.com/in/blurquin" target="_blank">
+      <div>
+        <BsLinkedin />
+      </div>
+    </a>
+  </div>
+);
+
+export default SocialMedia;
